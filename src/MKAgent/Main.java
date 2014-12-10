9b9f48bc1642e856ebe574e5e31c9a63d8b99149
andreeaPascu <andreea.pascu91@gmail.com>
2014-12-10 18:12:58 +0000

    added the final version - added to awesome branch: swap, move ordering with one lookahead and decommented heuristics

--- src/MKAgent/Main.java
+++ src/MKAgent/Main.java
@@ -15,47 +15,48 @@ import java.io.FileNotFoundException;
  */
 public class Main
 {
-    private static int noHoles = 7;
-    private static int noSeeds = 7;
-    private static int ai_depth = 5;
-    private static boolean first;
-    private static Side side;
-    private static Board globalBoard = new Board(noHoles, noSeeds);
+	private static int noHoles = 7;
+	private static int noSeeds = 7;
+	private static int ai_depth = 9;
+	private static boolean first;
+	private static Side side;
+	private static Board globalBoard = new Board(noHoles, noSeeds);
+	private static boolean isFirstMove = true;
+    private static Board initialBoard;
 
+	private static Reader input = new BufferedReader(new InputStreamReader(System.in));
 
-    private static Reader input = new BufferedReader(new InputStreamReader(System.in));
+	/**
+	 * Sends a message to the game engine.
+	 * @param msg The message.
+	 */
+	public static void sendMsg (String msg)
+	{
+		System.out.print(msg);
+		System.out.flush();
+	}
 
-    /**
-     * Sends a message to the game engine.
-     * @param msg The message.
-     */
-    public static void sendMsg (String msg)
-    {
-    	System.out.print(msg);
-    	System.out.flush();
-    }
+	/**
+	 * Receives a message from the game engine. Messages are terminated by
+	 * a '\n' character.
+	 * @return The message.
+	 * @throws IOException if there has been an I/O error.
+	 */
+	public static String recvMsg() throws IOException
+	{
+		StringBuilder message = new StringBuilder();
+		int newCharacter;
 
-    /**
-     * Receives a message from the game engine. Messages are terminated by
-     * a '\n' character.
-     * @return The message.
-     * @throws IOException if there has been an I/O error.
-     */
-    public static String recvMsg() throws IOException
-    {
-    	StringBuilder message = new StringBuilder();
-    	int newCharacter;
-
-    	do
-    	{
-    		newCharacter = input.read();
-    		if (newCharacter == -1)
-    			throw new EOFException("Input ended unexpectedly.");
-    		message.append((char)newCharacter);
-    	} while((char)newCharacter != '\n');
+		do
+		{
+			newCharacter = input.read();
+			if (newCharacter == -1)
+				throw new EOFException("Input ended unexpectedly.");
+			message.append((char)newCharacter);
+		} while((char)newCharacter != '\n');
 
 		return message.toString();
-    }
+	}
 
 
 	/**
@@ -64,52 +65,67 @@ public class Main
 	 */
 	public static void main(String[] args) throws Exception
 	{
-        PrintStream console = System.err;
-        File file = new File("output.txt");
-        FileOutputStream fos = new FileOutputStream(file);
-        PrintStream ps = new PrintStream(fos);
-        System.setErr(ps);
-        String s;
-        while (true)
-        {
-            System.err.println();
-            s = recvMsg();
-            System.err.print("Received: " + s);
-            MsgType mt = Protocol.getMessageType(s);
-            switch (mt)
-            {
-                case START: System.err.println("A start.");
-                    first = Protocol.interpretStartMsg(s);
-                    System.err.println("Starting player? " + first);
-                    side = (first) ? Side.SOUTH : Side.NORTH;
-                    if(first)
-                    {
-                        Move move = AlphaBeta.getBestMove(ai_depth, new State(true, globalBoard, side, null));
-                        sendMsg(Protocol.createMoveMsg(move.getHole()));
-                    }
-                    break;
-                case STATE: System.err.println("A state.");
-                    Protocol.MoveTurn r = Protocol.interpretStateMsg (s, globalBoard);
-                    System.err.println("This was the move: " + r.move);
-                    System.err.println("Is the game over? " + r.end);
-                    // If swapped
-                    if (r.move == -1) {
-                        side = side.opposite();
-                    }
-                    if (r.again) {
-                        Move move1 = AlphaBeta.getBestMove(ai_depth, new State(true, globalBoard, side, null));
-                        Move move2 = MiniMax.getBestMove(ai_depth, new State(true, globalBoard, side, null));
-                        // Move move2 = move1;
-                        if (move1.getHole() == move2.getHole())
-                            sendMsg(Protocol.createMoveMsg(move2.getHole()));
-                        else
-                            System.out.println("Wtf");
-                    }
-                    if (!r.end) System.err.println("Is it our turn again? " + r.again);
-                    System.err.print("The board:\n" + globalBoard);
-                    break;
-                case END: System.err.println("An end. Bye bye!"); return;
-            }
-        }
-    }
+		PrintStream console = System.err;
+		File file = new File("output.txt");
+		FileOutputStream fos = new FileOutputStream(file);
+		PrintStream ps = new PrintStream(fos);
+		System.setErr(ps);
+		String s;
+
+		while (true)
+		{
+			//System.err.println();
+			s = recvMsg();
+			//System.err.print("Received: " + s);
+			MsgType mt = Protocol.getMessageType(s);
+			switch (mt)
+			{
+				case START: System.err.println("A start.");
+					first = Protocol.interpretStartMsg(s);
+					//System.err.println("Starting player? " + first);
+					side = (first) ? Side.SOUTH : Side.NORTH;
+					if(first)
+					{
+						Move move = AlphaBeta.getBestMove(ai_depth, new State(true, globalBoard, side, null));
+						sendMsg(Protocol.createMoveMsg(move.getHole()));
+					}
+					else
+						initialBoard = globalBoard.clone();
+					break;
+
+				case STATE: //System.err.println("A state.");
+							//System.err.println("my side is " + side);
+
+							Protocol.MoveTurn r = Protocol.interpretStateMsg (s, globalBoard);
+							// If it's my opponent's turn and he makes his best move, we SWAP
+							if (!first && isFirstMove) {
+								isFirstMove = false;
+								Move bestFirstMove = AlphaBeta.getBestMove(ai_depth, new State(false, initialBoard, side, null));
+								if (bestFirstMove.getHole() == r.move) {
+									sendMsg(Protocol.createSwapMsg());
+									side = side.opposite();
+									break;
+								}
+							}
+
+							System.err.println("This was the move: " + r.move);
+							System.err.println("Is the game over? " + r.end);
+
+							// If swapped
+							if (r.move == -1) {
+								side = side.opposite();
+							}
+							if (r.again) {
+								Move move = AlphaBeta.getBestMove(ai_depth, new State(true, globalBoard, side, null));
+								sendMsg(Protocol.createMoveMsg(move.getHole()));
+							}
+
+							if (!r.end) System.err.println("Is it our turn again? " + r.again);
+							break;
+
+				case END: //System.err.println("An end. Bye bye!"); 
+						return;
+			}
+		}
+	}
 }
--- src/MKAgent/State.java
+++ src/MKAgent/State.java
@@ -1,135 +1,153 @@
 package MKAgent;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class State {
-    public boolean isMyTurn;
-    public Side mySide;
-    // This is the move that led to the state of board
-    public Move move;
-    public Board board;
+public class State implements Comparable<State> {
+	public boolean isMyTurn;
+	public Side mySide;
+	// This is the move that led to the state of board
+	public Move move;
+	public Board board;
+	public double score;
 
-    public State(boolean isMyTurn, Board board, Side side, Move move) {
-        this.board = board;
-        this.isMyTurn = isMyTurn;
-        this.mySide = side;
-        this.move = move;
+	public State(boolean isMyTurn, Board board, Side side, Move move) {
+		this.board = board;
+		this.isMyTurn = isMyTurn;
+		this.mySide = side;
+		this.move = move;
+		this.score = 0.0;
+	}
+
+	public boolean isEndState() {
+		return Kalah.gameOver(this.board);
+	}
+
+	public double evaluate() {
+		double score = 0;
+		double myStoreSeeds = board.getSeedsInStore(mySide);
+		double oppStoreSeeds = board.getSeedsInStore(mySide.opposite());
+
+		// Heuristic 1
+		score = 2 * ((isMyTurn) ? myStoreSeeds - oppStoreSeeds : oppStoreSeeds - myStoreSeeds);
+
+		// Heuristic 2
+		for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
+		    if (board.getSeeds(mySide, hole) == 0 && isSeedable(mySide, hole))
+		        if (isMyTurn)
+		            score += board.getSeedsOp(mySide, hole) / 2.0;
+		        else
+		            score -= board.getSeedsOp(mySide, hole) / 2.0;
+
+		// Heuristic 3
+		for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
+		    if (board.getSeeds(mySide.opposite(), hole) == 0 && isSeedable(mySide.opposite(), hole))
+		        if (isMyTurn)
+		            score -= board.getSeedsOp(mySide.opposite(), hole) / 2.0;
+		        else
+		            score += board.getSeedsOp(mySide.opposite(), hole) / 2.0;
+
+
+        //Heuristic 4
+		if (myStoreSeeds + oppStoreSeeds >= 40) {
+
+		    int mySideSeeds = 0, oppSideSeeds = 0;
+		    for (int i = 1; i <= board.getNoOfHoles(); i++) {
+		        mySideSeeds += board.getSeeds(mySide, i);
+		        oppSideSeeds += board.getSeeds(mySide.opposite(), i);
+		    }
+		    score += ((isMyTurn) ? (mySideSeeds - oppSideSeeds) : (oppSideSeeds - mySideSeeds));
+		}
+
+		// Heuristic 5
+		for (int i = 1; i <= board.getNoOfHoles(); i++) {
+		    if (board.getNoOfHoles() - i + 1 == board.getSeeds(mySide, i))
+		        if (isMyTurn)
+		            score += 2;
+		        else
+		            score -= 2;
+		}
+
+		// Heuristic 6
+		score += leftMinusRight(board,mySide);
+		score -= leftMinusRight(board,mySide.opposite());
+
+		return score;
+	}
+
+	public ArrayList<State> getChildStates() throws Exception {
+		ArrayList<State> states = new ArrayList<State>(7);
+
+		for (int hole = 1; hole <= board.getNoOfHoles(); hole++) {
+			Move move = new Move(mySide, hole);
+			if (Kalah.isLegalMove(board, move)) {
+				Board newBoard = board.clone();
+				Side side = Kalah.makeMove(newBoard, move);
+				State aState;
+				if (side == mySide){
+					aState = new State(isMyTurn, newBoard, side, move);
+					//states.add(new State(isMyTurn, newBoard, side, move));
+				}
+				else{
+					aState = new State(!isMyTurn, newBoard, side, move);
+					//states.add(new State(!isMyTurn, newBoard, side, move));
+				}
+				aState.score = evaluate();
+				states.add(aState);
+			}
+		}
+
+		Collections.sort(states);
+		return states;
+	}
+
+	public boolean isSeedable(Side side, int hole)
+	{
+		// Left side
+		for (int i = hole - 1; i > 0; i--)
+			if (hole - i == board.getSeeds(side, i))
+				return true;
+		// Right side
+		for (int i = hole + 1; i <= 7; i++)
+		{
+			if (15 - (i - hole) == board.getSeeds(side, i))
+				return true;
+		}
+		return false;
+	}
+	public double evaluateEndState() {
+		double myStoreSeeds = board.getSeedsInStore(mySide);
+		double oppStoreSeeds = board.getSeedsInStore(mySide.opposite());
+		if (isMyTurn)
+			return (myStoreSeeds > oppStoreSeeds) ? 1000 : myStoreSeeds - oppStoreSeeds;
+		else
+			return (oppStoreSeeds > myStoreSeeds) ? 1000 : oppStoreSeeds - myStoreSeeds;
+	}
+
+	public static int leftMinusRight(Board board, Side side)
+	{
+		int leftSeeds = 0, rightSeeds = 0;
+
+		// GET LEFT SEEDS ( 1 to size/2 )
+		for (int i = 1; i <=3; i++)
+			leftSeeds += board.getSeeds(side, i);
+
+		// Ignore the middle one
+
+		// GET RIGHT SEEDS ( size/2 to size )
+		for (int i = 5; i <= 7; i++)
+			rightSeeds += board.getSeeds(side, i);
+		return leftSeeds - rightSeeds;
+
+	}
+
+	@Override
+    public int compareTo(State otherState) {
+        return (this.score < otherState.score ) ? 1: (this.score > otherState.score) ? -1 : 0 ;
     }
 
-    public boolean isEndState() {
-        return Kalah.gameOver(this.board);
+    @Override
+    public String toString() {
+        return "" + score;
     }
-
-    public double evaluate() {
-        double score = 0;
-        double myStoreSeeds = board.getSeedsInStore(mySide);
-        double oppStoreSeeds = board.getSeedsInStore(mySide.opposite());
-
-        score = 2 *((isMyTurn) ? myStoreSeeds - oppStoreSeeds : oppStoreSeeds - myStoreSeeds);
-
-        // for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
-        //     if (board.getSeeds(mySide, hole) == 0 && isSeedable(mySide, hole))
-        //         if (isMyTurn)
-        //             score += board.getSeedsOp(mySide, hole) / 2.0;
-        //         else
-        //             score -= board.getSeedsOp(mySide, hole) / 2.0;
-
-        // for (int hole = 1; hole <= board.getNoOfHoles(); hole++)
-        //     if (board.getSeeds(mySide.opposite(), hole) == 0 && isSeedable(mySide.opposite(), hole))
-        //         if (isMyTurn)
-        //             score -= board.getSeedsOp(mySide.opposite(), hole) / 2.0;
-        //         else
-        //             score += board.getSeedsOp(mySide.opposite(), hole) / 2.0;
-
-
-
-        // if (myStoreSeeds + oppStoreSeeds >= 40) {
-
-        //     int mySideSeeds = 0, oppSideSeeds = 0;
-        //     for (int i = 1; i <= board.getNoOfHoles(); i++) {
-        //         mySideSeeds += board.getSeeds(mySide, i);
-        //         oppSideSeeds += board.getSeeds(mySide.opposite(), i);
-        //     }
-        //     score += ((isMyTurn) ? (mySideSeeds - oppSideSeeds) : (oppSideSeeds - mySideSeeds));
-        // }
-
-        // for (int i = 1; i <= board.getNoOfHoles(); i++) {
-        //     if (board.getNoOfHoles() - i + 1 == board.getSeeds(mySide, i))
-        //         if (isMyTurn)
-        //             score += 2;
-        //         else
-        //             score -= 2;
-        // }
-
-        // score += leftMinusRight(board,mySide);
-        // score -= leftMinusRight(board,mySide.opposite());
-
-        // System.err.println("Evaluation:");
-        // System.err.println("==================");
-        // System.err.println("The board: ");
-        // System.err.println(board);
-        // System.err.println("My turn? :" + isMyTurn);
-        // System.err.println("Side :" + mySide);
-        // System.err.println("Score: " + score);
-        // System.err.println("==================");
-
-        return score;
-    }
-
-    public ArrayList<State> getChildStates() throws Exception {
-        ArrayList<State> states = new ArrayList<State>(7);
-
-        for (int hole = 1; hole <= board.getNoOfHoles(); hole++) {
-            Move move = new Move(mySide, hole);
-            if (Kalah.isLegalMove(board, move)) {
-                Board newBoard = board.clone();
-                Side side = Kalah.makeMove(newBoard, move);
-                if (side == mySide)
-                    states.add(new State(isMyTurn, newBoard, side, move));
-                else
-                    states.add(new State(!isMyTurn, newBoard, side, move));
-            }
-        }
-        return states;
-    }
-
-    public boolean isSeedable(Side side, int hole)
-    {
-        // Left side
-        for (int i = hole - 1; i > 0; i--)
-            if (hole - i == board.getSeeds(side, i))
-                return true;
-        // Right side
-        for (int i = hole + 1; i <= 7; i++)
-        {
-            if (15 - (i - hole) == board.getSeeds(side, i))
-                return true;
-        }
-        return false;
-    }
-    public double evaluateEndState() {
-        double myStoreSeeds = board.getSeedsInStore(mySide);
-        double oppStoreSeeds = board.getSeedsInStore(mySide.opposite());
-        if (isMyTurn)
-            return (myStoreSeeds > oppStoreSeeds) ? 1000 : myStoreSeeds - oppStoreSeeds;
-        else
-            return (oppStoreSeeds > myStoreSeeds) ? 1000 : oppStoreSeeds - myStoreSeeds;
-    }
-
-    public static int leftMinusRight(Board board, Side side)
-      {
-            int leftSeeds = 0, rightSeeds = 0;
-
-            // GET LEFT SEEDS ( 1 to size/2 )
-            for (int i = 1; i <=3; i++)
-                leftSeeds += board.getSeeds(side, i);
-
-            // Ignore the middle one
-
-            // GET RIGHT SEEDS ( size/2 to size )
-            for (int i = 5; i <= 7; i++)
-                rightSeeds += board.getSeeds(side, i);
-            return leftSeeds - rightSeeds;
-
-      }
 }
